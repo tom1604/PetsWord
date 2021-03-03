f9944a4feefcb68a4739012587b663262b09fa9a
tom1604 <minaeva-toma-ya@yandex.ru>
2021-03-03 15:04:17 +0300

    Add AutoFac

---
+++ PetsWorld/Web/Integration/AutofacConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Web.Compilation;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+
+namespace Web
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            // Register your MVC controllers.
+            containerBuilder.RegisterControllers(Assembly.GetExecutingAssembly());
+            containerBuilder.RegisterFilterProvider();
+            var asseamblyServices = Assembly.Load("Business");
+            var asseamblyInterfaces = Assembly.Load("Interfaces");
+
+            containerBuilder.RegisterControllers(Assembly.GetExecutingAssembly()).PropertiesAutowired().InstancePerLifetimeScope();
+            containerBuilder.RegisterAssemblyTypes(asseamblyServices).AsImplementedInterfaces().PropertiesAutowired().InstancePerLifetimeScope();
+            containerBuilder.RegisterAssemblyTypes(asseamblyInterfaces).AsSelf().PropertiesAutowired().InstancePerLifetimeScope();
+
+            var container = containerBuilder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
